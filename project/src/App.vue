5e67c94c5cce32302ab56edf9b8f26f573df58a1
zml103 <2777826077@qq.com>
2022-11-06 01:15:33 +0800

    发布页面

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
     <!-- 首页 -->
-    <index v-show="false"></index>
+    <index v-show="false" ></index>
     <!-- 购物车 -->
-    <shoppingCar v-show="false"></shoppingCar>
+    <shoppingCar v-show="false" ></shoppingCar>
+    <!-- 发布商品 -->
+    <release v-show="true" ></release>
+
     <!-- 全部商品展示 -->
-    <commodity v-show="false"></commodity>
+    <commodity v-show="false" ></commodity>
     <!-- 用户中心 -->
-    <user v-show="true"></user>
+    <user v-show="false"></user>
   </div>
 </template>
 
 <script>
 import index from  './components/index'
 import shoppingCar from './components/shoppingCar'
+import release from './components/release'
 import commodity from './components/commodity'
 import user from './components/user'
 export default {
   name: 'App',
-  components:{index,shoppingCar,commodity,user}
+  components:{index,shoppingCar,commodity,user,release},
 }
+
 </script>
 
---
+++ project/src/components/release/releaseBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-row class="rBtn">
+        <el-button type="success">发布</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'ReleaseBtn',
+
+}
+</script>
+
+<style>
+/* 发布按钮样式 */
+.rBtn{
+  /* 发布按钮位置 */
+  position: absolute;
+  top:20px;
+  left: 60px;
+  /* 发布按钮大小 */
+  display: inline-block;
+  font-size:60px;
+}
+</style>
+发布页面
